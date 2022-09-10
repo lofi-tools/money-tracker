8fd363d2a19d30ea759e399b5f306756d76b6ceb
Roberto Aviles <roberto.aviles@gmail.com>
2022-09-10 19:35:32 +0100

    get user staking products

--- src/binance.rs
+++ src/binance.rs
@@ -1,97 +1,109 @@
 use hmac_sha256::HMAC;
-use reqwest::Url;
-use std::{lazy::Lazy, time::SystemTime};
+use reqwest::{Request, RequestBuilder, Url};
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
+use std::lazy::Lazy;
+use std::time::SystemTime;
 
+use crate::binance::staking::StakingPosition;
 use crate::utils::hex;
 
-const API_BASE: &'static str = "https://api.binance.com";
+const API_BASE: Lazy<Url> = Lazy::new(|| Url::parse("https://api.binance.com").unwrap());
 const API_KEY: Lazy<String> = Lazy::new(|| std::env::var("BINANCE_API_KEY").unwrap());
 const API_SECRET: Lazy<String> = Lazy::new(|| std::env::var("BINANCE_SECRET_KEY").unwrap());
+// const API_BASE: &'static str = "https://api.binance.com";
+// const API_KEY: &'static str = "BINANCE_API_KEY";
+// const API_SECRET: &'static str = "BINANCE_SECRET_KEY";
 
-pub async fn list_products() -> Result<(), Box<dyn std::error::Error>> {
-    // let client = reqwest::Client::new();
-    let url = Url::parse(&format!("{API_BASE}/sapi/v1/staking/productList"))?;
-    req_signed(url).await?;
-    // let resp = client
-    //     .get(url)
-    //     .header("X-MBX-APIKEY", &*API_KEY)
-    //     .send()
-    //     .await?;
-    // let resp_text = resp.text().await?;
-
-    // println!("{:#?}", resp_text);
-    Ok(())
+#[derive(Deserialize, Debug)]
+pub struct StakingProduct {
+    #[serde(rename = "projectId")]
+    project_id: String,
+    detail: StakingProductDetail,
+    quota: serde_json::Value,
+}
+#[derive(Deserialize, Debug)]
+pub struct StakingProductDetail {
+    asset: String, // Lock up asset
+    #[serde(rename = "rewardAsset")]
+    reward_asset: String, // Earn Asset
+    duration: u32, // Lock period(days)
+    renewable: bool, // Project supports renewal
+    #[serde(deserialize_with = "crate::utils::de_from_str")]
+    apy: f64, // APY in multiple_per_year,
 }
 
-async fn req_signed(url: Url) -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
-    let mut req = client
-        .get(url)
-        .header("X-MBX-APIKEY", &*API_KEY)
-        .query(&[("product", "STAKING")])
-        .build()?;
-
-    let mut url = req.url_mut();
-    // append timestamp
-    {
-        let timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)?
-            .as_millis();
-        dbg!(timestamp);
-        let mut query_pairs = url.query_pairs_mut();
-        query_pairs.append_pair("timestamp", &timestamp.to_string());
-    }
-
-    // then hmac query
-    let query_str = url.query().unwrap(); // TODO err
-    let signature = HMAC::mac(query_str, &*API_SECRET);
-    let sig_hex = hex(signature)?;
-
-    // then append hmac signature
-    {
-        let mut query_pairs = url.query_pairs_mut();
-        query_pairs.append_pair("signature", &sig_hex);
-    }
-
-    // let client = reqwest::Client::new();
-    // let req =
-    let resp = client.execute(req).await?;
-    let resp_text = resp.text().await?;
-
-    println!("{:#?}", resp_text);
-
-    todo!()
-}
-
-// TODO mv methods to this struct
 pub struct BinanceClient {
     httpc: reqwest::Client,
 }
-impl BinanceClient {}
+impl BinanceClient {
+    pub fn new() -> Self {
+        return BinanceClient {
+            httpc: reqwest::Client::new(),
+        };
+    }
+    pub fn get(&self, path: &str) -> Result<RequestBuilder, anyhow::Error> {
+        Ok(self.httpc.get(API_BASE.join(path)?))
+    }
+    #[deprecated]
+    fn signed(req_bld: RequestBuilder) -> Result<Request, anyhow::Error> {
+        let mut req = req_bld.header("X-MBX-APIKEY", &*API_KEY).build()?;
+        let url = req.url_mut();
 
-async fn list_swap_pools() {
-    let url = format!("{API_BASE}/sapi/v1/bswap/pools");
-}
+        // append timestamp
+        {
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)?
+                .as_millis();
+            let mut query_pairs = url.query_pairs_mut();
+            query_pairs.append_pair("timestamp", &timestamp.to_string());
+        }
 
-// TODO not pub, rm
-pub fn hmac() -> Result<(), Box<dyn std::error::Error>> {
-    let mut url = Url::parse("https://example.net")?;
-    let mut query_pairs = url.query_pairs_mut();
-    // for pair in query.iter() {
-    //     dbg!(pair);
-    // }
+        // then hmac query
+        let query_str = url.query().unwrap(); // TODO err
+        let signature = HMAC::mac(query_str, &*API_SECRET);
+        let sig_hex = hex(signature)?;
 
-    let timestamp = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)?
-        .as_secs();
-    query_pairs.append_pair("timestamp", &timestamp.to_string());
-    let query_str = query_pairs.finish().query().unwrap();
-    dbg!(query_str);
+        // then append hmac signature
+        {
+            let mut query_pairs = url.query_pairs_mut();
+            query_pairs.append_pair("signature", &sig_hex);
+        }
 
-    // Calculate the signature from the data and key
-    let signature = HMAC::mac(query_str, &*API_SECRET);
-    dbg!(hex(signature)?);
-    Ok(())
+        Ok(req)
+    }
+
+    // ROUTE METHODS
+    pub async fn list_all_products(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let url = API_BASE.join("/sapi/v1/staking/productList")?;
+        let req_bld = self.httpc.get(url).query(&[("product", "STAKING")]);
+        let req = Self::signed(req_bld)?;
+
+        let resp = self.httpc.execute(req).await?;
+        let resp_deser: Vec<StakingProduct> = resp.json().await?;
+
+        // match resp_deser {
+        //     serde_json::Value::Array(arr) => {
+        //         dbg!(arr.get(0));
+        //     }
+        //     _ => todo!(),
+        // }
+
+        dbg!(resp_deser.get(0));
+
+        todo!()
+    }
+
+    pub async fn list_user_products(&self) -> Result<(), anyhow::Error> {
+        let req = self.get("/sapi/v1/staking/position")?.sign()?;
+        let resp: Vec<StakingPosition> = self.get_resp(req).await?;
+
+        todo!()
+    }
+    pub async fn get_resp<D: DeserializeOwned>(&self, req: Request) -> Result<D, anyhow::Error> {
+        Ok(self.httpc.execute(req).await?.json().await?)
+        // TODO deserialize into BinanceErr if err status, return typed err if fails
+    }
 }
 
 struct BinanceApiErr {
@@ -111,10 +123,162 @@ mod tests {
         let key = b"NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j";
         let signature = HMAC::mac(data, key);
         let hex = hex(signature)?;
-        // println!("signature={:02x?}", signature);
 
         let expected = "c8db56825ae71d6d79447849e617115f4a920fa2acdcab2b053c4b2838bd6b71";
         assert_eq!(hex, expected);
         Ok(())
+    }
+}
+
+mod staking {
+    use serde::Deserialize;
+
+    #[allow(non_snake_case)]
+    #[derive(Deserialize)]
+    pub struct StakingPosition {
+        pub positionId: String,
+        pub projectId: String,
+        pub asset: String,
+        pub amount: String,
+        pub purchaseTime: String,
+        pub duration: String,
+        pub accrualDays: String,
+        pub rewardAsset: String,
+        pub APY: String,
+        pub rewardAmt: String,
+        pub extraRewardAsset: String,
+        pub extraRewardAPY: String,
+        pub estExtraRewardAmt: String,
+        pub nextInterestPay: String,
+        pub nextInterestPayDate: String,
+        pub payInterestPeriod: String,
+        pub redeemAmountEarly: String,
+        pub interestEndDate: String,
+        pub deliverDate: String,
+        pub redeemPeriod: String,
+        pub redeemingAmt: String,
+        pub canRedeemEarly: bool,
+        pub renewable: bool,
+        pub partialAmtDeliverDate: String,
+        pub status: String,
+    }
+}
+
+mod deprecated {
+    #[deprecated]
+    pub async fn list_products() -> Result<(), Box<dyn std::error::Error>> {
+        // // let client = reqwest::Client::new();
+        // // let url = Url::parse(&format!("{API_BASE}/sapi/v1/staking/productList"))?;
+        // let url = API_BASE.join("/sapi/v1/staking/productList")?;
+
+        // req_signed(url).await?;
+        // // let resp = client
+        // //     .get(url)
+        // //     .header("X-MBX-APIKEY", &*API_KEY)
+        // //     .send()
+        // //     .await?;
+        // // let resp_text = resp.text().await?;
+
+        // // println!("{:#?}", resp_text);
+        todo!()
+    }
+    // #[deprecated]
+    // async fn req_signed(url: Url) -> Result<(), Box<dyn std::error::Error>> {
+    //     // let client = reqwest::Client::new();
+    //     // // TODO mv staking out
+    //     // let mut req = client
+    //     //     .get(url)
+    //     //     .header("X-MBX-APIKEY", &*API_KEY)
+    //     //     .query(&[("product", "STAKING")])
+    //     //     .build()?;
+
+    //     // let mut url = req.url_mut();
+
+    //     // // append timestamp
+    //     // {
+    //     //     let timestamp = SystemTime::now()
+    //     //         .duration_since(SystemTime::UNIX_EPOCH)?
+    //     //         .as_millis();
+    //     //     dbg!(timestamp);
+    //     //     let mut query_pairs = url.query_pairs_mut();
+    //     //     query_pairs.append_pair("timestamp", &timestamp.to_string());
+    //     // }
+
+    //     // // then hmac query
+    //     // let query_str = url.query().unwrap(); // TODO err
+    //     // let signature = HMAC::mac(query_str, &*API_SECRET);
+    //     // let sig_hex = hex(signature)?;
+
+    //     // // then append hmac signature
+    //     // {
+    //     //     let mut query_pairs = url.query_pairs_mut();
+    //     //     query_pairs.append_pair("signature", &sig_hex);
+    //     // }
+
+    //     // let resp = client.execute(req).await?;
+    //     // let resp_text = resp.text().await?;
+    //     // // TODO NEXT TIME deser into struct
+    //     // // .json::<HashMap<String, String>>()
+
+    //     // println!("{:#?}", resp_text);
+
+    //     todo!()
+    // }
+    #[deprecated]
+    async fn list_swap_pools() {
+        // let url = format!("{API_BASE}/sapi/v1/bswap/pools");
+    }
+
+    #[deprecated]
+    fn hmac() -> Result<(), Box<dyn std::error::Error>> {
+        // let mut url = Url::parse("https://example.net")?;
+        // let mut query_pairs = url.query_pairs_mut();
+        // // for pair in query.iter() {
+        // //     dbg!(pair);
+        // // }
+
+        // let timestamp = SystemTime::now()
+        //     .duration_since(SystemTime::UNIX_EPOCH)?
+        //     .as_secs();
+        // query_pairs.append_pair("timestamp", &timestamp.to_string());
+        // let query_str = query_pairs.finish().query().unwrap();
+        // dbg!(query_str);
+
+        // // Calculate the signature from the data and key
+        // let signature = HMAC::mac(query_str, &*API_SECRET);
+        // dbg!(hex(signature)?);
+        todo!()
+    }
+}
+
+pub trait RequestBuilderExt {
+    fn sign(self) -> Result<Request, anyhow::Error>;
+}
+impl RequestBuilderExt for RequestBuilder {
+    fn sign(self) -> Result<Request, anyhow::Error> {
+        let mut req = self.header("X-MBX-APIKEY", &*API_KEY).build()?;
+        let url = req.url_mut();
+
+        // append timestamp
+        {
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)?
+                .as_millis();
+            let mut query_pairs = url.query_pairs_mut();
+            query_pairs.append_pair("timestamp", &timestamp.to_string());
+        }
+
+        // then hmac query
+        let query_str = url.query().unwrap(); // TODO err
+        let signature = HMAC::mac(query_str, &*API_SECRET);
+        let sig_hex = hex(signature)?;
+
+        // then append hmac signature
+        {
+            let mut query_pairs = url.query_pairs_mut();
+            query_pairs.append_pair("signature", &sig_hex);
+        }
+
+        Ok(req)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,129 @@
 #![feature(once_cell)]
 
 mod binance;
+use binance::BinanceClient;
 mod utils;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv()?;
 
-    // TODO load binance API key, query products/subs
+    let bc = BinanceClient::new();
+    bc.list_all_products().await?;
+    // TODO update Products, UserProducts with binance data
 
-    // let client = reqwest::Client::new();
-    // let resp = client
-    //     .get("https://api.binance.com/sapi/v1/staking/productList")
-    //     .header(
-    //         "X-MBX-APIKEY",
-    //         "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A",
-    //     )
-    //     .send()
-    //     .await?;
-    // let resp_text = resp.text().await?;
-
-    // let resp = reqwest::get("https://api.binance.com/sapi/v1/staking/productList")
-    //     .await?
-    //     // .json::<HashMap<String, String>>()
-    //     .text()
-    //     .await?;
-
-    binance::list_products().await?;
-    // binance::hmac()?;
     Ok(())
+}
+
+mod models {
+    use crate::data::{AssetId, ProductId, ASSETS, PRODUCTS};
+    #[derive(Debug)]
+    pub struct Asset {
+        pub id: String,
+    }
+
+    #[derive(Debug)]
+    pub struct Product {
+        pub id: String,
+        pub asset_id: AssetId,
+    }
+    impl Product {
+        pub fn asset(&self) -> &'static Asset {
+            let asset = ASSETS.get(&self.asset_id).unwrap();
+            asset
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct UserPosition {
+        // pub product: &'a Product<'a>,
+        pub product_id: ProductId,
+        pub amount: f64,
+    }
+    impl UserPosition {
+        pub fn product(&self) -> Option<&'static Product> {
+            let p = PRODUCTS.get(&self.product_id);
+            p
+        }
+    }
+}
+
+mod data {
+    use crate::models::{Asset, Product};
+    use std::collections::HashMap;
+
+    lazy_static::lazy_static! {
+        pub static ref ASSETS: ListAssets = fetch_assets();
+        pub static ref PRODUCTS: ListProducts = fetch_products();
+    }
+
+    // pub const ASSETS: Lazy<ListAssets> = Lazy::new(|| fetch_assets());
+    // pub const PRODUCTS: Lazy<ListProducts> = Lazy::new(|| fetch_products());
+
+    fn fetch_assets() -> ListAssets {
+        let mut assets = ListAssets::new();
+        assets.insert("ETH");
+        assets.insert("DOT");
+        assets.insert("NEAR");
+        assets.insert("CRO");
+        assets
+    }
+
+    fn fetch_products() -> ListProducts {
+        let mut products = ListProducts::new();
+        // TODO fetch from Binance
+
+        // let assets: &ListAssets = &*ASSETS;
+        // let eth: &Asset = (*ASSETS).get("ETH").unwrap();
+        // static asset: Asset = ASSETS.get("ETH").unwrap();
+        // let product: Product<'static> = Product {
+        //     id: "TODO".to_string(),
+        //     asset: asset,
+        // };
+        // products.insert(product);
+        products
+    }
+
+    #[derive(PartialEq, Eq, Hash, Debug)]
+    pub struct AssetId(pub String);
+    pub struct ListAssets {
+        map: HashMap<AssetId, Asset>,
+    }
+    impl ListAssets {
+        pub fn new() -> Self {
+            ListAssets {
+                map: HashMap::new(),
+            }
+        }
+        pub fn insert(&mut self, asset_id: &str) {
+            self.map.insert(
+                AssetId(asset_id.to_string()),
+                Asset {
+                    id: asset_id.to_string(),
+                },
+            );
+        }
+        pub fn get<'a>(&'a self, id: &AssetId) -> Option<&'a Asset> {
+            self.map.get(id)
+        }
+    }
+
+    #[derive(PartialEq, Eq, Hash, Debug)]
+    pub struct ProductId(pub String);
+    pub struct ListProducts {
+        map: HashMap<ProductId, Product>,
+    }
+    impl ListProducts {
+        pub fn new() -> Self {
+            ListProducts {
+                map: HashMap::new(),
+            }
+        }
+        pub fn insert(&mut self, product: Product) {
+            self.map.insert(ProductId(product.id.clone()), product);
+        }
+        pub fn get<'a>(&'a self, id: &ProductId) -> Option<&'a Product> {
+            self.map.get(id)
+        }
+    }
 }
